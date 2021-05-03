ddb747316046e659266edbc374ef560245008b0d
Vivek Patel <vivekpatel2422@gmail.com>
2021-05-03 12:26:27 -0400

    Add search feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Card from "./components/card/Card";
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false")
@@ -15,13 +16,26 @@ function App() {
       .catch((error) => console.error(error));
   }, []);
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="app">
       <div className="header">
         <h1 className="name"><i className="fas fa-chart-line"></i>CryptoTrack</h1>
       </div>
+      <div className="search">
+        <form>
+          <input className="searchField" type="text" onChange={handleChange} placeholder=" Search a cryptocurrency (Ex. Bitcoin)" />
+        </form>
+      </div>
       <div className="cardContainer">
-        {coins.map((coin) => {
+        {filteredCoins.map((coin) => {
           return (
             <Card
               key={coin.id}
