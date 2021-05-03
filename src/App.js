8e2af051d19866776f43d450d55886a1c968caeb
Vivek Patel <vivekpatel2422@gmail.com>
2021-05-03 01:29:51 -0400

    Add cards with data from API using axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import './App.css';
 import Card from "./components/card/Card";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <Card />
-    </div>
+    <div className="app">
+      <div className="header">
+        <h1 className="name"><i className="fas fa-chart-line"></i>CryptoTrack</h1>
+      </div>
+      <div className="cardContainer">
+        {coins.map((coin) => {
+          return (
+            <Card
+              key={coin.id}
+              image={coin.image}
+              name={coin.name}
+              symbol={coin.symbol}
+              price={coin.current_price}
+              priceChange={coin.price_change_percentage_24h}
+              marketCap={coin.market_cap}
+              marketCapRank={coin.market_cap_rank}
+              volume={coin.total_volume}
+              high={coin.high_24h}
+              low={coin.low_24h}
+              circulatingSupply={coin.circulating_supply}
+            />
+          );
+        })}
+      </div>
+    </div >
   );
 }
 
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,24 +1,28 @@
 import React from "react";
 import "./Card.css";
 
-function Card() {
+const Card = ({ image, name, symbol, price, priceChange, marketCap, marketCapRank, volume, high, low, circulatingSupply }) => {
     return (
         <div className="coin">
             <div className="coinHeadDiv">
-                <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" alt="logo" className="coinLogo" />
-                <h1 className="coinName">Bitcoin</h1>
-                <p className="coinSymbol">BTC</p>
+                <img src={image} alt={`${name}`} className="coinLogo" />
+                <h1 className="coinName">{name}</h1>
+                <p className="coinSymbol">{symbol}</p>
             </div>
             <div className="coinPriceDiv">
-                <p className="coinPrice">$5</p>
-                <p className="coinPriceChange">+2%</p>
+                <p className="coinPrice">${price.toLocaleString()} CAD</p>
+                {priceChange > 0 ? (
+                    <p className="coinPriceChangeUp">+{priceChange.toFixed(2)}%</p>
+                ) : (
+                    <p className="coinPriceChangeDown">{priceChange.toFixed(2)}%</p>
+                )}
             </div>
-            <p className="coinMarketCap">Market Cap: $100000</p>
-            <p className="coinMarketCapRank">Market Cap Rank: 1</p>
-            <p className="coinVolume">24H Volume: $2500</p>
-            <p className="coinHigh">24H High: $6</p>
-            <p className="coinLow">24H Low: $4</p>
-            <p className="coinCirculatingSupply">Circulating Supply: 15000</p>
+            <p className="coinMarketCap">Market Cap: ${marketCap.toLocaleString()}</p>
+            <p className="coinMarketCapRank">Market Cap Rank: {marketCapRank.toLocaleString()}</p>
+            <p className="coinVolume">24H Volume: ${volume.toLocaleString()}</p>
+            <p className="coinHigh">24H High: ${high.toLocaleString()}</p>
+            <p className="coinLow">24H Low: ${low.toLocaleString()}</p>
+            <p className="coinCirculatingSupply">Circulating Supply: {circulatingSupply.toLocaleString()}</p>
         </div>
     );
 };
