57fa1beca7ef49488a1003d4ae6df252fb685dc3
Vivek Patel <vivekpatel2422@gmail.com>
2021-05-02 23:01:39 -0400

    Add card styling and axios

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,20 +1,24 @@
 import React from "react";
+import "./Card.css";
 
 function Card() {
     return (
         <div className="coin">
-            <div className="coinNameDiv">
-                <h1 className="coinName">Name</h1>
-                <p className="coinSymbol">Symbol</p>
+            <div className="coinHeadDiv">
+                <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" alt="logo" className="coinLogo" />
+                <h1 className="coinName">Bitcoin</h1>
+                <p className="coinSymbol">BTC</p>
             </div>
-            <p className="coinPrice">$5</p>
-            <p className="coinMarketcap">Market Cap: $100000</p>
+            <div className="coinPriceDiv">
+                <p className="coinPrice">$5</p>
+                <p className="coinPriceChange">+2%</p>
+            </div>
+            <p className="coinMarketCap">Market Cap: $100000</p>
+            <p className="coinMarketCapRank">Market Cap Rank: 1</p>
             <p className="coinVolume">24H Volume: $2500</p>
             <p className="coinHigh">24H High: $6</p>
             <p className="coinLow">24H Low: $4</p>
-            <div className="priceChangeDiv">
-                <p className="priceChange">2%</p>
-            </div>
+            <p className="coinCirculatingSupply">Circulating Supply: 15000</p>
         </div>
     );
 };
